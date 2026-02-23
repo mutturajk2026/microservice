20dc9e05d21b25ee068b0373a3989a6a7e6315b0
Mutturaj <rajkumarkadlimatti19@gmail.com>
2026-02-23 19:43:16 +0530

    committing KafkaProducer class

--- src/main/java/com/codedecode/KafkaDemo/service/KafkaProducer.java
+++ src/main/java/com/codedecode/KafkaDemo/service/KafkaProducer.java
@@ -4,14 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class KafkaProducer {
 
-	@Autowired
-	private KafkaTemplate<String, String> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
-	public void sendMessageToTopic(String message) {
-		kafkaTemplate.send("CodeDecodeTopic", message);
-	}
-
+    public void sendMessageToTopic(String message) {
+        kafkaTemplate.send("CodeDecodeTopic", message);
+    }
 }
+
+List<String> names= arrays.asList("India", "Usa", "UK");
+names.Stream().map(name -> names.toUpperCase()).forEach(n-> system.out.println(n));
